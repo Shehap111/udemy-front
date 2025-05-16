5e8d6c73f841a7edc62ee0d6031fee42148c9405
Shehapsamer <https://github.com/Shehapsamer>
2025-05-16 16:30:25 +0300

    update loclal storeg

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,6 @@ const nextConfig = {
   images: {
     domains: ['example.com', 'res.cloudinary.com'], // إضافة الدومين هنا
   },
-  i18n: {
-    locales: ['en', 'ar', 'de', 'es'],  // قائمة اللغات المتاحة
-    defaultLocale: 'en',                 // اللغة الافتراضية
-  },
 };
 
 export default nextConfig;
